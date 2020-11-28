6ad3171a3585cc3d9bf471b8fcc6c4dd645decc6
AsemAmmar99 <asemammar99@gmail.com>
2020-11-28 23:23:17 +0200

    FAQs and FAQs Answers Screens Done

---
+++ app/src/main/java/com/scorpion_a/htigp/model/FAQsListData.java
@@ -0,0 +1,17 @@
+package com.scorpion_a.htigp.model;
+
+public class FAQsListData {
+    private String faqTitle;
+
+    public FAQsListData(String faqTitle) {
+        this.faqTitle = faqTitle;
+    }
+
+    public String getFaqTitle() {
+        return faqTitle;
+    }
+
+    public void setFaqTitle(String faqTitle) {
+        this.faqTitle = faqTitle;
+    }
+}
