cadf9a29449e4ca53cb89aebfc00fed17927c8fa
AsemAmmar99 <asemammar99@gmail.com>
2021-02-07 16:59:23 +0200

    Linking

--- app/src/main/java/com/scorpion_a/studentapp/activities/StudentProfile.kt
+++ app/src/main/java/com/scorpion_a/studentapp/activities/StudentProfile.kt
@@ -1,10 +1,13 @@
 package com.scorpion_a.studentapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import com.scorpion_a.studentapp.R
 import kotlinx.android.synthetic.main.activity_profile_page.*
+import kotlinx.android.synthetic.main.activity_profile_page.header
+import kotlinx.android.synthetic.main.activity_student_profile.*
 
 class StudentProfile : AppCompatActivity() {
     lateinit var toolbar: Toolbar
@@ -13,5 +16,15 @@ class StudentProfile : AppCompatActivity() {
         setContentView(R.layout.activity_student_profile)
         toolbar=header.findViewById(R.id.toolbar)
         toolbar.title="Profile Page"
+
+        ivEdit.setOnClickListener {
+            val intent = Intent(this, StudentEditProfile::class.java)
+            startActivity(intent)
+        }
+
+        tvEditPass.setOnClickListener {
+            val intent = Intent(this, EditPasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
